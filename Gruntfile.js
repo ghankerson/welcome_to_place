fc86f2db10e709445655c618c5360793fe4d08f2
Geoff Hankerson <ghankerson@mpr.org>
2015-05-05 20:57:16 -0500

    More gruntfile setup

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,4 +1,5 @@
 module.exports = function(grunt) {
+  'use strict';
   require('load-grunt-tasks')(grunt); // npm install --save-dev load-grunt-tasks
 
   grunt.initConfig({
@@ -11,8 +12,38 @@ module.exports = function(grunt) {
           'app/styles/components/components.css': 'app/styles/components/components.scss'
         }
       }
+    },
+    cssmin: {
+      options: {
+        sourceMap: true
+      },
+      target: {
+        files: {
+          'app/styles/components/main.min.css': ['app/styles/components/components.css']
+        }
+      }
+    },
+    jshint: {
+      all: ['Gruntile.js','app/scripts/**/*.js']
+    },
+    imageoptim: {
+      optimize: {
+        src: ['app/images']
+      },
+      jpegs: {
+        src: ['app/images/**/*.jpg', 'app/images/**/*.JPG']
+      }
+    },
+    clean: ['dist'],
+    copy: {
+      main: {
+        files: [
+          {expand: false, src: ['app/styles/components/main.min.css'], dest: 'dist/assets/css/main.min.css'},
+          {expand: false, src: ['app/styles/components/main.min.css.map'], dest: 'dist/assets/css/main.min.css.map'},
+        ]
+      }
     }
   });
 
-  grunt.registerTask('default', ['sass']);
+  grunt.registerTask('default', ['sass', 'jshint', 'cssmin', 'clean', 'copy']);
 };
