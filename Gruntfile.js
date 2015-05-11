55b945f412263cac2bba922ec8b57c3eb5fd8d7f
Geoff Hankerson <ghankerson@mpr.org>
2015-05-10 19:39:05 -0500

    More grunt setup

--- Gruntfile.js
+++ Gruntfile.js
@@ -34,16 +34,49 @@ module.exports = function(grunt) {
         src: ['app/images/**/*.jpg', 'app/images/**/*.JPG']
       }
     },
-    clean: ['dist'],
+    //clean: ['dist'],
     copy: {
       main: {
         files: [
           {expand: false, src: ['app/styles/components/main.min.css'], dest: 'dist/assets/css/main.min.css'},
           {expand: false, src: ['app/styles/components/main.min.css.map'], dest: 'dist/assets/css/main.min.css.map'},
+          {expand: false, src: ['app/scripts/main.js'], dest: 'dist/assets/js/main.js'},
+          {expand: false, src: ['app/index.html'], dest: 'dist/index.html'},
+          {expand: true, cwd: 'app/images', src: ['**'], dest: 'dist/assets/images/'},
         ]
+      }
+    },
+    watch: {
+      scripts: {
+        files: ['./app/scripts/**/*.js'],
+        tasks: ['jshint', 'newer:copy'],
+        options: {
+          livereload: true
+        }
+      },
+      styles: {
+        files: ['./app/styles/**/*.scss'],
+        tasks: ['sass', 'cssmin','newer:copy'],
+        options: {
+          livereload: true
+        }
+      },
+      markup: {
+        files: ['./app/index.html'],
+        tasks: ['newer:copy']
+      }
+    },
+    'http-server': {
+      dev: {
+        root: 'dist',
+        port: 3000,
+        host: '0.0.0.0',
+        showDir: true,
+        autoIndex: true,
+        runInBackground: true
       }
     }
   });
 
-  grunt.registerTask('default', ['sass', 'jshint', 'cssmin', 'clean', 'copy']);
+  grunt.registerTask('default', ['newer:sass', 'newer:jshint', 'newer:cssmin', 'newer:copy']);
 };
